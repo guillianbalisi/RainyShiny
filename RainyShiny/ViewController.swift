0cbfc572f2c768ab824f6db03859a91e3cffdac1
guillianbalisi <guillianbalisi@gmail.com>
2017-03-13 15:24:56 -0400

    Initial Commit

---
+++ RainyShiny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainyShiny
+//
+//  Created by Guillian Balisi on 2017-03-13.
+//  Copyright © 2017 Guillian Balisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
